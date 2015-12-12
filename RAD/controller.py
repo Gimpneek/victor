77c8914a2bcd30d980a5b3bbb2cabc04f343e873
Colin Wren <colin@gimpneek.com>
2015-12-12 23:21:46 +0000

    Adding reading of .victor.yml file

--- RAD/controller.py
+++ RAD/controller.py
@@ -11,6 +11,7 @@ CLIENT_SECRET = os.environ.get('CLIENTSECRET')
 
 @app.route('/', methods=['GET'])
 def homepage():
+	# Check if we have an access token, if not redirect to login page
 	access_token = request.cookies.get('gat')
 	if access_token:
 		github = Github(login_or_token=access_token)
@@ -23,20 +24,29 @@ def homepage():
 
 @app.route('/<github_account>/<repo>', methods=['GET'])
 def repo_homepage(github_account=None, repo=None):
+	# Check if we have access token, repo and account, if not redirect to login
 	access_token = request.cookies.get('gat')
 	if not github_account or not repo or not access_token:
 		return make_response(redirect('/'))
+	# Get the logged in, repos for that user, requested account and repo
 	github = Github(login_or_token=access_token)
 	user = github.get_user()
 	repos = user.get_repos()
 	requested_account = github.get_user(github_account)
 	requested_repo = requested_account.get_repo(repo)
+
+	# Read the .victor file if it has one
+	try:
+		victor_file = requested_repo.get_contents('.victor.yml').decoded_content
+	except:
+		victor_file = None
 	return render_template(
 			'repo_homepage.html',
 			user=user,
 			repos=repos,
 			requested_account=requested_account,
-			requested_repo=requested_repo
+			requested_repo=requested_repo,
+			victor_file=victor_file
 	)
 
 
