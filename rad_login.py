9dd9422da78d9590b2225c51e6efdf3e02f53cf7
Colin Wren <colin@gimpneek.com>
2015-12-12 21:48:55 +0000

    Making flow with github a little easier

--- rad_login.py
+++ rad_login.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, make_response, redirect
 from github import Github
 import os
 import requests
@@ -10,25 +10,32 @@ CLIENT_SECRET=os.environ.get('CLIENTSECRET')
 
 @app.route('/')
 def homepage():
-	return '<a href="https://github.com/login/oauth/authorize?scope=user:email&client_id={0}">Sign in with GitHub</a>'.format(CLIENT_ID)
+	access_token = request.cookies.get('gat')
+	if access_token:
+		g = Github(login_or_token=access_token)
+		repos = ''
+		for repo in g.get_user().get_repos():
+			repos += '<li>{0}</li>'.format(repo.name)
+		return '<ul>{0}</ul>'.format(repos)
+	else:
+		return '<a href="https://github.com/login/oauth/authorize?scope=user:email&client_id={0}">Sign in with GitHub</a>'.format(CLIENT_ID)
 
 @app.route('/callback', methods=['GET'])
 def repos():
 	session_code = request.args.get('code')
-	get_access_token = requests.post('https://github.com/login/oauth/access_token',
-	                                 data={
-		                                 'client_id': CLIENT_ID,
-		                                 'client_secret': CLIENT_SECRET,
-		                                 'code': session_code,
-		                                 'accept': 'application/json'
-	                                 })
+	get_access_token = requests.post(
+			'https://github.com/login/oauth/access_token',
+			data={
+				'client_id': CLIENT_ID,
+				'client_secret': CLIENT_SECRET,
+				'code': session_code,
+				'accept': 'application/json'
+			}
+	)
 	access_token = urlparse.parse_qs(get_access_token.content).get('access_token')[0]
-	g = Github(login_or_token=access_token)
-	repos = ''
-	for repo in g.get_user().get_repos():
-		repos += '<li>{0}</li>'.format(repo.name)
-	return '<ul>{0}</ul>'.format(repos)
-
+	response = make_response(redirect('/'))
+	response.set_cookie('gat', access_token)
+	return response
 
 if __name__ == "__main__":
 	app.run(port=8000)
