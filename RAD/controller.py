aecf4c4d2e1205431756890785ea1fb21c5d500c
Colin Wren <colin@gimpneek.com>
2015-12-13 22:35:07 +0000

    Adding connection to database and creating user and repos

--- RAD/controller.py
+++ RAD/controller.py
@@ -1,12 +1,44 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask import render_template
+from flask import g
 from github import Github
+from github import BadCredentialsException
 import os
 import requests
 import urlparse
+import sqlite3
+
+# Create Application
 app = Flask(__name__)
 
+# Configuration
 CLIENT_ID = os.environ.get('CLIENTID')
 CLIENT_SECRET = os.environ.get('CLIENTSECRET')
+DATABASE = os.path.join(app.root_path, 'database.db')
+DEBUG = False
+SECRET_KEY = 'dev key'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+app.config.from_object(__name__)
+
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
 
 
 @app.route('/', methods=['GET'])
@@ -17,7 +49,56 @@ def homepage():
 		github = Github(login_or_token=access_token)
 		user = github.get_user()
 		repos = user.get_repos()
-		return render_template('post_login.html', user=user, repos=repos)
+		active_repos = []
+		# Check to see if GitHub user already has an account in database
+		check_cur = g.db.execute(
+				'select id from user where github_id = ?',
+				[user.id]
+		)
+		existing_user = check_cur.fetchone()
+		# If not create one and set flag to show first login page
+		if not existing_user:
+			add_user = g.db.execute(
+					'insert into user (github_id) values (?)',
+					[user.id]
+			)
+			g.db.commit()
+			user_id = add_user.lastrowid
+			for repo in repos:
+				g.db.execute(
+						'insert into repo (user_id, repo_id) values (?, ?)',
+						[user_id,repo.id]
+				)
+			g.db.commit()
+		else:
+			existing_user = existing_user[0]
+			# grab list of the active repos for user in database
+			db_act_repos = g.db.execute(
+					'select repo_id from repo where user_id = ? '
+					'and victor_active = 1',
+					[existing_user]
+			)
+			act_repos = db_act_repos.fetchall()
+			for repo in repos:
+				if repo.id in act_repos:
+					active_repos.append(repo)
+		# If not show off repo list
+		try:
+			return render_template(
+					'post_login.html',
+					user=user,
+					repos=repos,
+					active_repos=active_repos
+			)
+		except BadCredentialsException:
+			response = make_response(
+					render_template(
+						'landing_page.html',
+						github_client_id=CLIENT_ID
+					)
+			)
+			response.set_cookie('gat', '', expires=0)
+			return response
 	else:
 		return render_template('landing_page.html', github_client_id=CLIENT_ID)
 
@@ -34,7 +115,6 @@ def repo_homepage(github_account=None, repo=None):
 	repos = user.get_repos()
 	requested_account = github.get_user(github_account)
 	requested_repo = requested_account.get_repo(repo)
-
 	# Read the .victor file if it has one
 	try:
 		victor_file = requested_repo.get_contents('.victor.yml').decoded_content
