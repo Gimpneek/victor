7a3b98e7804c6af0180c1ae20cef9aeb0d7cbe00
Colin Wren <colin@gimpneek.com>
2016-04-23 22:17:51 +0100

    Adding branch test for step

--- rad/static/src/js/__tests__/step-test.js
+++ rad/static/src/js/__tests__/step-test.js
@@ -21,11 +21,31 @@ const TEST_STEP = {
         }]
     },
     location: "example.feature:8"
-}
+};
+
+const TEST_FAILED_STEP = {
+    name: "I have \"2\" apples",
+    keyword: "Given",
+    step_type: "given",
+    result: {
+        status: "failed",
+        duration: 0.0001289844512939453
+    }
+};
+
+const TEST_NO_RESULT = {
+    name: "I have \"2\" apples",
+    keyword: "Given",
+    step_type: "given",
+};
 
 describe('Step', () => {
     const step = TestUtils.renderIntoDocument(<Step step={TEST_STEP}/>);
+    const failedStep = TestUtils.renderIntoDocument(<Step step={TEST_FAILED_STEP}/>);
+    const noResultStep = TestUtils.renderIntoDocument(<Step step={TEST_NO_RESULT}/>);
     const stepNode = ReactDOM.findDOMNode(step);
+    const failedStepNode = ReactDOM.findDOMNode(failedStep);
+    const noResultStepNode = ReactDOM.findDOMNode(noResultStep);
 
     it('Shows the keyword from the Step object', () => {
         const keywordNode = stepNode.getElementsByTagName('strong')[0];
@@ -36,8 +56,18 @@ describe('Step', () => {
         expect(stepNode.textContent).toBe('Given I have "2" apples');
     });
 
-    it('Shows the icon for the step', () => {
+    it('Shows the icon for the step - passed', () => {
         const iconNode = stepNode.getElementsByTagName('i')[0];
         expect(iconNode.getAttribute('class')).toBe('fa fa-check');
+    });
+
+    it('Shows the icon for the step - failed', () => {
+        const iconNode = failedStepNode.getElementsByTagName('i')[0];
+        expect(iconNode.getAttribute('class')).toBe('fa fa-exclamation');
+    });
+
+    it('Shows the icon for the step - no result', () => {
+        const iconNode = noResultStepNode.getElementsByTagName('i')[0];
+        expect(iconNode.getAttribute('class')).toBe('fa fa-exclamation');
     });
 });
